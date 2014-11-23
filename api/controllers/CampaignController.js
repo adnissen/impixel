d3e66b285aefb308b46d2c47d34836967847e9f5
adnissen <andrew_nissen@yahoo.com>
2014-11-23 10:51:58 -0500

    Made the dashboard show the number of impressions

--- api/controllers/CampaignController.js
+++ api/controllers/CampaignController.js
@@ -23,6 +23,13 @@ module.exports = {
           userId: req.param('uid'),
           random: req.param('r')
         }).exec(function createCB(data, created){
+          Campaign.findOne(campaignId, function findOneCB(e, campaign){
+            if (campaign.impressions)
+              campaign.impressions = campaign.impressions + 1;
+            else
+              campaign.impressions = 1;
+            campaign.save();
+          });
           return res.ok();
         });
       }
